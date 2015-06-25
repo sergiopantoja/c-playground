041510aa5485b5540deaa37b80d114922fb08f62
Sergio Pantoja <sergio@sergiopantoja.com>
2015-06-25 18:40:57 -0400

    :star2: Add pointer sizes to sizeof() test

--- sizeof.c
+++ sizeof.c
@@ -8,6 +8,11 @@ int main()
   printf("long:\t%lu bytes\n", sizeof(long));
   printf("float:\t%lu bytes\n", sizeof(float));
   printf("double:\t%lu bytes\n", sizeof(double));
+  printf("\npointers\n\n");
+  printf("pointer to void:\t%lu bytes\n", sizeof(void *));
+  printf("pointer to char:\t%lu bytes\n", sizeof(char *));
+  printf("pointer to int: \t%lu bytes\n", sizeof(int *));
+  printf("pointer to long:\t%lu bytes\n", sizeof(long *));
 
   return 0;
 }
